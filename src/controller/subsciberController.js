15680ec07a45f396370078ad71d3a86c7c51d3d6
Rajputsachin01 <rajputsachin8878@gmail.com>
2025-06-14 14:31:06 +0530

    addded subscriber module

--- src/controller/subsciberController.js
+++ src/controller/subsciberController.js
@@ -1,23 +1,33 @@
 const SubscriberModel = require("../models/subscriberModel");
+const Helper = require("../utils/helper");
+
 const createSubscriber = async (req, res) => {
   try {
     const { email } = req.body;
 
-    if (!email) return Helper.fail(res, "email  is required");
+    if (!email) return Helper.fail(res, "Email is required");
+
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!emailRegex.test(email)) {
       return Helper.fail(res, "Email is not valid!");
     }
-    const subscriberCreated = await SubscriberModel.create({
-      email,
-    });
-    if (!subscriberCreated) {
-      return Helper.fail(res, "Not Subscibed");
+
+    const existingSubscriber = await SubscriberModel.findOne({ email });
+    if (existingSubscriber) {
+      return Helper.fail(res, "Email already subscribed, please use another email.");
     }
-    return Helper.success(res, "Subscibed successfully", subscriberCreated);
+
+    const subscriberCreated = await SubscriberModel.create({ email });
+
+    if (!subscriberCreated) {
+      return Helper.fail(res, "Subscription failed");
+    }
+
+    return Helper.success(res, "Subscribed successfully", subscriberCreated);
   } catch (error) {
     console.log(error);
     return Helper.fail(res, error.message);
   }
 };
+
 module.exports= {createSubscriber}
